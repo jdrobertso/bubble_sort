3fb7c7f1103918858ceb735a3fd89e9737be3f37
Jeremy <jrobertson.88@gmail.com>
2017-08-23 19:30:17 -0500

    Added pseudocode and method

---
+++ bubble_sort.rb
@@ -0,0 +1,14 @@
+=begin
+for each element in an array, check each item against the next item.
+if the next item is larger than the first, swap them
+do this one less time than the length of the array
+only need to check the number of items up to the number of items minus number of passes
+if number of swaps = 0, you're done sortinggi
+=end
+
+def bubble_sort(arr)
+  x = arr.length
+  for x do
+
+  end
+end
